308011d71e94fd9423e26af7de9cb93296228335
Kenneth Rebello <kennethrebello253@gmail.com>
2024-09-22 16:24:08 +0530

    updated cart controller

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,12 +1,14 @@
 import express from "express"
 import { addToCart, removeFromCart, getCart } from "../controllers/cartController.js"
+import authMiddleware from "../middleware/auth.js";
 
+// router configuration
 const cartRouter = express.Router();
 
 //endpoints
 
-cartRouter.post("/add", addToCart)
-cartRouter.post("/remove",removeFromCart)
-cartRouter.post("/get", getCart)
+cartRouter.post("/add",authMiddleware, addToCart)
+cartRouter.post("/remove",authMiddleware, removeFromCart)
+cartRouter.post("/get",authMiddleware, getCart)
 
 export default cartRouter;
