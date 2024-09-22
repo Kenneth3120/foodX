0d7f4130ab151cf921a4a21b00f5ea7e7cf3d0b7
Kenneth Rebello <kennethrebello253@gmail.com>
2024-09-22 16:32:01 +0530

    added vercel.json

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,13 @@ const port = 4000
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-lwhq.vercel.app"],
+        methods: ["GET", "POST"],
+        credentials: true,
+    }
+))
 
 //db Connection
 connectDB();
