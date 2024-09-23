6634ba1169d25e80fb33c0cff04e7967b6b0de61
Kenneth Rebello <kennethrebello253@gmail.com>
2024-09-23 19:42:45 +0530

    Update assets.js

--- admin/src/assets/admin_assets/assets.js
+++ admin/src/assets/admin_assets/assets.js
@@ -15,4 +15,4 @@ export const assets ={
 }
 
 // export const url = 'http://localhost:4000'
-export const url = 'https://food-x-akasa-back.vercel.app'
+export const url = 'https://foodx-back.onrender.com'
