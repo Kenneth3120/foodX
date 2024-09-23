2b886a7cb90010bd8c944f13a8ce9007d88dc904
Kenneth Rebello <kennethrebello253@gmail.com>
2024-09-23 21:41:03 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "https://food-x-admin.vercel.app"
+  const url = "https://foodx-back.onrender.com"
   return (
     <div>
     <ToastContainer />
