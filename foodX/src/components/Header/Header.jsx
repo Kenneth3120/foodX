c54626d05ad726ed5e0cfa0122558df5f1a5ca62
Kenneth Rebello <kennethrebello253@gmail.com>
2024-09-26 10:50:30 +0530

    Update Header.jsx

--- foodX/src/components/Header/Header.jsx
+++ foodX/src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <div className='header-contents'>
         <h2>Order your favourite food here!</h2>
         <p>Choose from the best menu ever on our airlines</p>
-        <p className="notice"><strong>Note:</strong> The website will take around 120 seconds to load.</p>
+        <p className="notice"><strong>Note:</strong> The website will take around 120 seconds to load due to its free hosting on "render". Works perfect on Localhost :)</p>
         <button onClick={handleClick}>View Menu</button> {/* Added onClick */}
       </div>
     </div>
